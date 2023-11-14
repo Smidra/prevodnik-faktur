30c3ae2839df5531c3e8250a066ffae7b4018f82
smidra <rad.smid@gmail.com>
2023-11-14 22:55:06 +0100

    feat: Nový ksicht.

--- new-ui/main.js
+++
@@ -1,6 +0,0 @@
-
-//... your script logic goes here
-
-document.getElementById('my-dropdown').addEventListener('cds-dropdown-selected', (evt) => {
-    console.log("SELECTED ITEM: ", evt.detail.item.value);
-})
